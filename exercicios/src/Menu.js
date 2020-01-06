23a909622025264750c36296c61605df4b70e2a2
Wesley de Oliveira de Paula <wpaula@spring.corp>
2020-01-06 17:22:55 -0300

    Added Contador, Plataformas and ValidarProps

--- exercicios/src/Menu.js
+++ exercicios/src/Menu.js
@@ -4,8 +4,20 @@ import { createDrawerNavigator } from 'react-navigation'
 import Simples from './componentes/Simples';
 import ParImpar from './componentes/ParImpar'
 import Inverter, { MegaSena } from './componentes/Multi'
+import Contador from './componentes/Contador'
+import Plataformas from './componentes/Plataformas'
+import ValidarProps from './componentes/ValidarProps'
 
 export default createDrawerNavigator({
+    ValidarProps: {
+        screen: () => <ValidarProps label="Year: " ano={20} />
+    },
+    Plataformas: {
+        screen: Plataformas
+    },
+    Contador: {
+        screen: () => <Contador numeroInicial={10} />
+    },
     MegaSena: {
         screen: () => <MegaSena numeros={6}/>,
         navigationOptions: { title: 'Mega Sena' }
